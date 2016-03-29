d079a5c212422a8bc01c6b675d362a74d30bb64d
Chad Cunningham <ccunningham2434@mail.kvcc.edu>
2016-03-28 20:03:26 -0400

    Add ADO Datatbase Entity. Update program to print all entries

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -10,6 +10,19 @@ namespace cis237inclass5
     {
         static void Main(string[] args)
         {
+            // >Make a new instance of the cars collection.
+            CarsCCunninghamEntities ccunninghamEntities = new CarsCCunninghamEntities();
+
+            //**************************************
+            // List out all of the cars in the table
+            //**************************************
+            Console.WriteLine("Print the list");
+
+            foreach (Car car in ccunninghamEntities.Cars)
+            {
+                Console.WriteLine(car.id + " " + car.model + " " + car.model);
+            }
+            Console.ReadLine();
         }
     }
 }
